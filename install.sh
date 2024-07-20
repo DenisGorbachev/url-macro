21097628113ccbbc96e0f7bfdad9c64ea3ec0765
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2024-07-20 14:33:11 +0700

    doc: validate urls

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+npm install --global \
+  lefthook@1.6.9 \
+  remark-cli@12.0.1 \
+  remark-validate-links@8.0.0 \
+  remark-lint-no-dead-urls@1.1.0
+
+# Install yj
+curl -L https://github.com/sclevine/yj/releases/download/v5.1.0/yj-linux-amd64 -o /tmp/yj
+chmod +x /tmp/yj
+sudo mv /tmp/yj /usr/local/bin/yj
