b67781e296c49cce5b8b322a930a84002c1cba8f
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2024-07-31 15:26:30 +0700

    misc: allow more types in commitlint

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,1 +1,30 @@
-export default { extends: ['@commitlint/config-conventional'] }
+const severity = {
+  disable: 0,
+  warn: 1,
+  error: 2,
+}
+
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      severity.error,
+      'always',
+      [
+        'build',
+        'chore',
+        'misc',
+        'other',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+      ],
+    ],
+  },
+}
--- install.sh
+++ install.sh
@@ -6,7 +6,8 @@ npm install --global \
   remark-validate-links@8.0.0 \
   remark-lint-no-dead-urls@1.1.0 \
   @commitlint/cli@19.3.0 \
-  @commitlint/config-conventional@19.2.2
+  @commitlint/config-conventional@19.2.2 \
+  @commitlint/types@19.0.3
 
 cargo install --git https://github.com/DenisGorbachev/cargo-doc2readme --branch dev
 cargo install cargo-machete
