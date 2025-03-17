4d1229c6b4137c1335a98009a5a3c8950cb3f909
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2025-03-17 16:49:07 +0700

    fix: ask for package descrition, install lefthook

--- .repoconf/hooks/post-init.sh
+++ .repoconf/hooks/post-init.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env -S usage bash
-#USAGE arg "<dir>"
 #USAGE flag "-n --name <name>" help="Package name (if this flag is not provided, then the package name is inferred from the directory name)"
+#USAGE arg "<dir>"
 
 set -xeuo pipefail
 
@@ -16,6 +16,8 @@ if [[ ! "$answer" = "Y" ]] || [[ "$answer" = "y" ]]; then
   exit 1
 fi
 
+read -r -p "Package description: " description
+
 (cd "$dir" && mise trust)
 (cd "$dir" && mise install)
 
@@ -25,11 +27,11 @@ name_new_snake_case=$(ccase --to snake "$name_new")
 repo_url=$(cd "$dir" && gh repo view --json url | jq -r .url)
 
 tomli set -f "$cargo_toml" "package.name" "$name_new" | sponge "$cargo_toml"
-tomli delete -f "$cargo_toml" "package.description" | sponge "$cargo_toml"
 tomli delete -f "$cargo_toml" "package.metadata.details.title" | sponge "$cargo_toml"
 tomli delete -f "$cargo_toml" "package.metadata.details.readme.generate" | sponge "$cargo_toml"
 tomli set -f "$cargo_toml" "package.repository" "$repo_url" | sponge "$cargo_toml"
 tomli set -f "$cargo_toml" "package.homepage" "$repo_url" | sponge "$cargo_toml"
+tomli set -f "$cargo_toml" "package.description" "$description" | sponge "$cargo_toml"
 
 while IFS= read -r file; do
   rm "$dir/$file"
@@ -39,6 +41,8 @@ done < "$dir/.repoconf/data/init-removed-files"
 set +e
 rg --files-with-matches "$name_old_snake_case" "$dir" | xargs gsed -i "s/\b$name_old_snake_case\b/$name_new_snake_case/g"
 set -e
+
+(cd "$dir" && mise exec "npm:lefthook" -- lefthook install)
 
 (cd "$dir" && mise run build)
 
